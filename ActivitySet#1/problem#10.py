8e42c77e658e784ccf85caa8b45292bc8cd71416
Aditya Nayar <94883383+adityanayar3@users.noreply.github.com>
2022-05-12 16:37:57 +0000

    ..

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,18 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+mail=dict{}
+for line in handle:
+    if not line.startswith("From"):
+        continue
+    word=line.split()
+    email=word[1]
+    if email in mail:
+        mail[email]=1+mail[email]
+    else:
+        mail.update({email:1})
+
