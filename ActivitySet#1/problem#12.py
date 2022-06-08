300c6fe2c0265953950752e7514871d84d50935b
Aditya Nayar <94883383+adityanayar3@users.noreply.github.com>
2022-06-08 10:49:52 +0000

    ..

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,9 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('[0-9]+', line)
+    if len(x) > 0:
+        print(x)
