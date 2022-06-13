714925132a3654bf172a4a5f8fb37323c46eb212
Aditya Nayar <94883383+adityanayar3@users.noreply.github.com>
2022-06-13 03:40:43 +0000

    ..

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -2,8 +2,10 @@
 # https://www.py4e.com/lessons/regex
 import re
 hand = open('mbox-short.txt')
+sum=0
 for line in hand:
     line = line.rstrip()
     x = re.findall('[0-9]+', line)
     if len(x) > 0:
-        print(x)
+        sum=sum+0
+print(sum)
