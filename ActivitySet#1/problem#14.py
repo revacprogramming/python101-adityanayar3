c7af65ad3d87485aeb6fb06989b043366e7d896b
Aditya Nayar <94883383+adityanayar3@users.noreply.github.com>
2022-07-06 09:55:21 +0000

    ..

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,7 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+cd 
--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,12 +1,14 @@
 
-
+lst=[]
 def get_cs():
     """get string input"""
+  str=input("Enter a string")
 
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
-
+  lst=str.split()
+  
 
 def main():
     cs = get_cs()
